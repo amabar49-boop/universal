fbc2520d1bb3bb35b00e3d649bb3afb5d334d610
amabar49-boop <amabar49@gmail.com>
2026-03-09 22:26:29 +0700

    Add new game URL for PlaceId 96645548064314

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local Games = {
     [71827913900569] = "https://raw.githubusercontent.com/amabar49-boop/vinzhub/refs/heads/main/beanstalk.lua",
     [82031770257269] = "https://raw.githubusercontent.com/amabar49-boop/jumbrenrott/refs/heads/main/lompatbrenrot.lua",
     [77073005043385]  = "https://raw.githubusercontent.com/amabar49-boop/alamak/refs/heads/main/main.lua"
+    [96645548064314]  = "https://raw.githubusercontent.com/amabar49-boop/vinzhub/refs/heads/main/main.lua"
 }
 
 local scriptUrl = Games[game.PlaceId]
