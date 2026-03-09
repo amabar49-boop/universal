5101b26d73d89d19fe143308c7f93bc22ddb5aed
amabar49-boop <amabar49@gmail.com>
2026-03-09 22:17:58 +0700

    Update game ID for beanstalk.lua URL

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local Games = {
-    [1773068815292] = "https://raw.githubusercontent.com/amabar49-boop/vinzhub/refs/heads/main/beanstalk.lua",
+    [71827913900569] = "https://raw.githubusercontent.com/amabar49-boop/vinzhub/refs/heads/main/beanstalk.lua",
     [2753915549] = "https://raw.githubusercontent.com/Silver/SilverHub/main/brenrot2.lua",
     [155615604]  = "https://raw.githubusercontent.com/Silver/SilverHub/main/brenrot3.lua"
 }
